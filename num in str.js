97991cce4508070ceda414c6bd48dec32d3aac2d
vasia88 <snoopy150688@yandex.ru>
2024-01-22 11:49:45 +0300

    число в  строку и в развернутом виде

---
+++ num in str.js
@@ -0,0 +1,16 @@
+function expandedForm(num) {
+    // Your code here
+     let numStr = num.toString().split('');
+     
+     for(let i = 0 ; i < numStr.length; i++ ){
+         
+         for(let y = numStr.length - i; y > 1; y--){
+            numStr[i] += '0'; 
+            // console.log(y);  use this to debug y, and no y value print out from console
+         }
+     }
+     
+    
+     numStr = numStr.filter(value => !value.startsWith(0));
+     return numStr.join(' + ')
+   }
