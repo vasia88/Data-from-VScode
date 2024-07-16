3c4c058dc2f741e89b0ecb74da03bef07a8d5e78
vasia88 <snoopy150688@yandex.ru>
2024-07-16 23:33:15 +0300

    seven

--- freecodecamp/JS 2/seven.js
+++ freecodecamp/JS 2/seven.js
@@ -6,9 +6,17 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-
-for (let i = 1; i <= count; i++) {
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
+}*/
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 < 10) {
+  console.log("5 is less than 10");
+} else {
+  console.log("This is the else block");
 }
 
 let result = ""
