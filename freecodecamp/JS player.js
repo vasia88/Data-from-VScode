063a5314df0b0127014f5f86d188080b4bf937c2
vasia88 <snoopy150688@yandex.ru>
2024-08-29 21:57:54 +0300

    player

--- freecodecamp/JS player.js
+++ freecodecamp/JS player.js
@@ -79,14 +79,13 @@ const allSongs = [
 ];
 
 const audio = new Audio();
+
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
 
-const printGreeting = () => {
-  console.log('Hello there!'); 
-}
-
-printGreeting();
+const renderSongs = (array) => {
+  const songsHTML = array.map(song) => ;
+};
