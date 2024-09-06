534b4da0b84c066298bfe6b56d6cf9f06e726c32
vasia88 <snoopy150688@yandex.ru>
2024-09-06 23:06:36 +0300

    calorie

--- freecodecamp/JS calorie/Calorie counter.js
+++ freecodecamp/JS calorie/Calorie counter.js
@@ -18,7 +18,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
@@ -31,3 +31,5 @@ function addEntry() {
   />`;
   targetInputContainer.innerHTML += HTMLString;
 }
+
+addEntryButton.addEventListener("click", addEntry);
