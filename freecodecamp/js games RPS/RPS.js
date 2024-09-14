ab19e4ce5d29ee2d3cb70ec3acdf287f37580ecb
vasia88 <snoopy150688@yandex.ru>
2024-09-14 23:14:39 +0300

    RPS

--- freecodecamp/js games RPS/RPS.js
+++ freecodecamp/js games RPS/RPS.js
@@ -1,15 +1,23 @@
-function getRandomComputerResult() {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-    return options[randomIndex];
-  }
-  
-  function hasPlayerWonTheRound(player, computer) {
-    if(player = "Rock" && computer = "Scissors") {
-      return true
-    }
-    
-  }
-  
-  console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
-  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
+function getRandomCfunction getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+ 
+}
+
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
