8546b40bd39a768958e73e13424fb231284ce30f
vasia88 <snoopy150688@yandex.ru>
2024-10-04 23:13:52 +0300

    player

--- freecodecamp/JS player.js
+++ freecodecamp/JS player.js
@@ -110,8 +110,17 @@ const renderSongs = (array) => {
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
 
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
   });
+
 };
 
 renderSongs(userData?.songs);
