6378bc4c38469051d8875398c3c263e80ca474d3
vasia88 <snoopy150688@yandex.ru>
2024-10-14 22:13:16 +0300

    player

--- freecodecamp/JS player.js
+++ freecodecamp/JS player.js
@@ -86,6 +86,14 @@ let userData = {
 };
 
 const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  }
+
 
 };
 
