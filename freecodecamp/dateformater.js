d42feda213ca973917a7dae583b310f0706df1f6
vasia88 <snoopy150688@yandex.ru>
2024-11-08 22:59:00 +0300

    date

--- freecodecamp/dateformater.js
+++ freecodecamp/dateformater.js
@@ -1,5 +1,11 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
 const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+
 
