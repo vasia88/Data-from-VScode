216ab10cadaf23bfb4fb473d598a23f76d8f5858
vasia88 <snoopy150688@yandex.ru>
2024-11-16 18:05:32 +0300

    date

--- freecodecamp/dateformater.js
+++ freecodecamp/dateformater.js
@@ -11,8 +11,10 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
+
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
-
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
   }
 });
