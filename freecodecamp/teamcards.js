3a515a19f147a1788e50e5ed3b2d67a6b6ed1e27
vasia88 <snoopy150688@yandex.ru>
2024-12-17 23:14:33 +0300

    team

--- freecodecamp/teamcards.js
+++ freecodecamp/teamcards.js
@@ -190,11 +190,11 @@ const setPlayerCards = (arr = players) => {
           <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
           <p>Position: ${position}</p>
           <p>Number: ${number}</p>
-          <p>Nickname: </p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
       `;
     }
-  );
+  )
 
 
 };
