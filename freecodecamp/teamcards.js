f5660f32bdf44b045daa25383ca20208f015bedb
vasia88 <snoopy150688@yandex.ru>
2024-12-25 00:15:28 +0300

    team

--- freecodecamp/teamcards.js
+++ freecodecamp/teamcards.js
@@ -198,13 +198,14 @@ const setPlayerCards = (arr = players) => {
 
 playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
-
-
+  
   switch (e.target.value) {
     case "nickname":
       setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+
+
+
 
   }
-
-
 });
