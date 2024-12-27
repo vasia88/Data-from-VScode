5392f96fd377595760d26251b8246b935e17d4d9
vasia88 <snoopy150688@yandex.ru>
2024-12-28 00:04:08 +0300

    TODO

---
+++ freecodecamp/todo.js
@@ -0,0 +1,4 @@
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+
