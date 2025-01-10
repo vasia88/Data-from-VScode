17f4bf9ab929619ce3e6b5486a43467e59e288f5
vasia88 <snoopy150688@yandex.ru>
2025-01-10 19:54:27 +0300

    TODO

--- freecodecamp/todo.js
+++ freecodecamp/todo.js
@@ -34,7 +34,7 @@ taskForm.addEventListener("submit", (e) => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
   const taskObj = {
-id: titleInput.value
+    id: titleInput.value.toLowerCase().split(" ")
   };
   console.log(taskObj);
 });
