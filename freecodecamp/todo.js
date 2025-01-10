2a28ef39f4023edc3c4b7cdba33577d212356c57
vasia88 <snoopy150688@yandex.ru>
2025-01-10 22:37:24 +0300

    TODO

--- freecodecamp/todo.js
+++ freecodecamp/todo.js
@@ -28,13 +28,14 @@ discardBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
 
+
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
   const taskObj = {
-    id: titleInput.value.toLowerCase().split(" ")
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    
   };
   console.log(taskObj);
 });
