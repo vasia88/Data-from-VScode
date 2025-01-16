8a3ada2deadd42dfd333a8685e01e65e61a62dd8
vasia88 <snoopy150688@yandex.ru>
2025-01-16 23:20:51 +0300

    TODO

--- freecodecamp/todo.js
+++ freecodecamp/todo.js
@@ -46,7 +46,10 @@ taskForm.addEventListener("submit", (e) => {
 
   taskData.forEach(({id, title, date, description}) => {
       tasksContainer.innerHTML += `
-       <div class="task" id="${id}"></div>   
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          
+        </div>
       `
     }
   );
