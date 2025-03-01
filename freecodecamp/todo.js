61e9b1f4a52d347f90c14e80ef422eef36d6b501
vasia88 <snoopy150688@yandex.ru>
2025-03-01 21:52:38 +0300

    TODO

--- freecodecamp/todo.js
+++ freecodecamp/todo.js
@@ -28,6 +28,7 @@ const addOrUpdateTask = () => {
     taskData[dataArrIndex] = taskObj;
   }
 
+  localStorage.setItem("data", JSON.stringify(taskData));
   updateTaskContainer()
   reset()
 };
@@ -58,6 +59,7 @@ const deleteTask = (buttonEl) => {
 
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
+
 }
 
 const editTask = (buttonEl) => {
@@ -111,19 +113,3 @@ taskForm.addEventListener("submit", (e) => {
 
   addOrUpdateTask();
 });
-
-const myTaskArr = [
-  { task: "Walk the Dog", date: "22-04-2022" },
-  { task: "Read some books", date: "02-11-2023" },
-  { task: "Watch football", date: "10-08-2021" },
-];
-
-localStorage.setItem("data", JSON.stringify(myTaskArr));
-
-
-
-const getTaskArr = localStorage.getItem("data")
-console.log(getTaskArr)
-
-const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
-console.log(getTaskArrObj);
