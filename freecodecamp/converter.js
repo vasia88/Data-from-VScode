5d4e3bec3b143e75e6ee914fdc5a4494cebf12d1
vasia88 <snoopy150688@yandex.ru>
2025-04-01 21:38:29 +0300

    conv

--- freecodecamp/converter.js
+++ freecodecamp/converter.js
@@ -3,11 +3,15 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
+
+
   console.log(numberInput.value);
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 
 numberInput.addEventListener("keydown", (e) => {
-  console.log(e);
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
