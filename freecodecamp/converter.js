f3acb3f67a4653c8fc44729fde98f1bdb55ea256
vasia88 <snoopy150688@yandex.ru>
2025-05-08 23:26:26 +0300

    conv

--- freecodecamp/converter.js
+++ freecodecamp/converter.js
@@ -11,7 +11,39 @@ const checkUserInput = () => {
     !numberInput.value ||
     isNaN(parseInt(numberInput.value)) ||
     parseInt(numberInput.value) < 0
-  ) {
+  ) {const numberInput = document.getElementById("number-input");
+    const convertBtn = document.getElementById("convert-btn");
+    const result = document.getElementById("result");
+    
+    const decimalToBinary = (input) => {
+      const inputs = [];
+      const quotients = [];
+      const remainders = [];
+    
+      
+    };
+    
+    const checkUserInput = () => {
+      if (
+        !numberInput.value ||
+        isNaN(parseInt(numberInput.value)) ||
+        parseInt(numberInput.value) < 0
+      ) {
+        alert("Please provide a decimal number greater than or equal to 0");
+        return;
+      }
+    
+      decimalToBinary(parseInt(numberInput.value));
+      numberInput.value = "";
+    };
+    
+    convertBtn.addEventListener("click", checkUserInput);
+    
+    numberInput.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        checkUserInput();
+      }
+    });
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
