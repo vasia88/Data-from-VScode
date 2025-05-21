0a9eae34eacebac2260645a253345abcfce9bef0
vasia88 <snoopy150688@yandex.ru>
2025-05-21 23:04:26 +0300

    conv

--- freecodecamp/converter.js
+++ freecodecamp/converter.js
@@ -8,9 +8,11 @@ const decimalToBinary = (input) => {
   const remainders = [];
 
   while (input > 0) {
-    const quotient = input / 2;
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
 
-    input = 0;
+
+    input = quotient;
   }
 };
 
