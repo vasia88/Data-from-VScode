316a371145e2fc642cd0d30b9aa63fea23a21781
vasia88 <snoopy150688@yandex.ru>
2025-05-23 23:58:09 +0300

    conv

--- freecodecamp/converter.js
+++ freecodecamp/converter.js
@@ -17,7 +17,11 @@ const decimalToBinary = (input) => {
     input = quotient;
   }
 
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
 
+  
 };
 
 const checkUserInput = () => {
